edc5fe0f2ce24a2a26251a3003503562b399432a
ponlawat-w <ponlawat_w@cmu.ac.th>
2018-08-10 17:03:36 +0700

    －男弦＆女弦を交換 －♯＆♭の機能

--- js/script.js
+++ js/script.js
@@ -12,7 +12,15 @@ const Note = {
         Note.notes = [];
         $(Note).trigger('change');
     },
-    toStrArr: () => Note.notes.map(note => KEY_NAME[note])
+    toStr: val => {
+        let note = KEY_NAME[val & 0b00111111];
+        let sharpFlat = val & 0b11000000;
+        if (sharpFlat) {
+            note += KEY_NAME[sharpFlat];
+        }
+        return note;
+    },
+    toStrArr: () => Note.notes.map(note => Note.toStr(note))
 };
 
 $(document).ready(() => {
@@ -23,16 +31,32 @@ $(document).ready(() => {
 
     const keyMapping = KEY_MAPPING.QWERTY;
 
-    $(this).keypress(e => {
-        const note = keyMapping[e.keyCode];
-        if (typeof note !== 'undefined') {
-            Note.add(note);
+    $(this).keydown(e => {
+        if (e.shiftKey || e.altKey) {
+            return false;
         }
     });
 
     $(this).keyup(e => {
         if (e.keyCode === 8) {
             Note.backspace();
+        } else if (e.keyCode === 32) {
+            Note.add(SANSHIN.PAUSE);
+        } else {
+            let note = keyMapping[e.keyCode] ? keyMapping[e.keyCode] : keyMapping[e.keyCode + 32];
+
+            if (typeof note !== 'undefined') {
+
+                if (!(e.shiftKey & e.altKey)) {
+                    if (e.shiftKey) {
+                        note |= SANSHIN.SHARP;
+                    } else if (e.altKey) {
+                        note |= SANSHIN.FLAT;
+                    }
+                }
+
+                Note.add(note);
+            }
         }
     });
 
